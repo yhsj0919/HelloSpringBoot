749cbd44a89cee5106df38d9fe07cfc2eda7da3d
yhsj <1130402124@qq.com>
2017-01-20 11:32:52 +0800

    修改404的错误拦截

--- src/main/java/xyz/yhsj/hello/HelloSpringBootApplication.java
+++ src/main/java/xyz/yhsj/hello/HelloSpringBootApplication.java
@@ -3,15 +3,23 @@ package xyz.yhsj.hello;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication//等同于 @Configuration @EnableAutoConfiguration @ComponentScanpublic
 public class HelloSpringBootApplication {
 
+    /**
+     * 处理json数据
+     */
 
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
@@ -22,6 +30,21 @@ public class HelloSpringBootApplication {
         fastConverter.setFastJsonConfig(fastJsonConfig);
         HttpMessageConverter<?> converter = fastConverter;
         return new HttpMessageConverters(converter);
+    }
+
+    /**
+     * 进行404处理
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
+//                container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500"));
+//                container.addErrorPages(new ErrorPage(java.lang.Throwable.class,"/error/500"));
+            }
+        };
     }
 
     public static void main(String[] args) {
